0904b32091109959f10785b862f464df178d48e5
prafull <prafull@unitednewdigitalmedia.com>
2024-12-08 10:43:28 +0530

    removed dtenv for deployment

--- src/client/main.py
+++ src/client/main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import os
-from dotenv import load_dotenv
-# Load environment variables from a .env file
-load_dotenv()
+# from dotenv import load_dotenv
+# # Load environment variables from a .env file
+# load_dotenv()
 from pages.chat import chat_bot
 from pages.upload import upload_page
 
